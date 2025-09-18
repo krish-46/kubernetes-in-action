04f354d4d17c7d226ce17face9d670367831cb54
CHANDAN MAHATO <85212943+krish-46@users.noreply.github.com>
2025-09-18 15:12:17 -0400

    Update app.py v2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello from Week3 Lab3!", 200
+    return "Hello from Week3 Lab3 v2!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
